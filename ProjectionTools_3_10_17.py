715bd5d6a878624b52a1b1bcd7b76bcbbed7ce98
electricsauce <spete008@gmail.com>
2017-03-14 17:12:52 -0700

    Added texture input and split UI into 2 distinct working areas. 1 for texture projection, 1 for displacement.
    git push origin master

--- ProjectionTools_3_10_17.py
+++ ProjectionTools_3_10_17.py
@@ -24,7 +24,7 @@ CC3 - Attribution required
 
 
 import bpy
-import os       #for texture file import
+import os                               #for texture file import
 import bmesh
 
 #math stuff
@@ -40,32 +40,61 @@ import time                              #For timing our functions to improve pe
 #Create GUI panel
 #multi-panel - http://blender.stackexchange.com/questions/41933/bl-context-multiple-areas
 ##############################################################################################
-class ProjectionToolsPanel(bpy.types.Panel):
+class ProToolsProjectionPanel(bpy.types.Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "TOOLS"
-    #bl_context = "objectmode"
     bl_category = "ProTools"
     bl_label = "Projection Tools"
 
     def draw(self, context):
-        scene = context.scene
+        layout = self.layout
+        #wm = context.window_manager
+        #scene = context.scene
         
         TheCol = self.layout.column(align=True)
+        
+        row = layout.row()
+        
+        #image preview - todo
+        #TheCol.template_preview( bpy.data.textures["bpy.context.ProT_ImageTexturePath"])
+        
+        TheCol.prop(context.scene,"ProT_ImageTexturePath")
+        
+        TheCol.prop(context.scene,"ProT_use_noise_tex")
+        if context.scene.ProT_use_noise_tex:
+            TheCol.prop(context.scene,"ProT_NoiseTexturePath")
+            
         TheCol.operator("mesh.prot_paint_vertices", text="Paint Vertices")
-        TheCol.operator("mesh.prot_displace_vertices", text="Displace Vertices")
-        TheCol.prop(context.scene, "ProT_rotate_texture_90")
+        TheCol.prop(context.scene,"ProT_rotate_texture_90")
         TheCol.prop(context.scene,"ProT_texture_scale")
-        TheCol.prop(context.scene,"ProT_disp_scale")
         TheCol.prop(context.scene,"ProT_Xnoise_scale")
         TheCol.prop(context.scene,"ProT_Ynoise_scale")
         
         
     #end draw
+    
+class ProToolsDisplacementPanel(bpy.types.Panel):
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_category = "ProTools"
+    bl_label = "Displacement Tools"
+    
+    def draw(self, context):
+        layout = self.layout
+        
+        TheCol = self.layout.column(align=True)
+                
+        TheCol.operator("mesh.prot_displace_vertices", text="Displace Vertices")
+        TheCol.operator("mesh.prot_displace_vertices_method2", text="Displace Vertices_no copy")
+        TheCol.prop(context.scene,"ProT_disp_scale")
+    
+#Allows us to use our panel in more than one context. Very helpful when applying
+#vertex colors
 contexts = ["objectmode", "vertexpaint"]
 
 for c in contexts:
     propdic = {"bl_idname": "ProTools.%s" % c, "bl_context": c, }
-    projectionToolsPanel = type("ProTools%s" % c, (ProjectionToolsPanel,),propdic)
+    projectionToolsPanel = type("ProTools%s" % c, (ProToolsProjectionPanel,),propdic)
 
 
 ##############################################################################################
@@ -77,9 +106,12 @@ class ProT_PaintVertices(bpy.types.Operator):
     bl_options = {"UNDO"}
     
     def invoke(self, context, event):
+        start = time.time()
         # Load image file. Change here if the snippet folder is 
         # not located in you home directory.
-        realpath = os.path.expanduser('~/Documents/Blender/ProjectionTools/WoodBase.png')
+        #realpath = os.path.expanduser('~/Documents/Blender/ProjectionTools/WoodBase.png')      
+        #realpath = os.path.expanduser(context.scene.ProT_ImageTexturePath)
+        realpath = context.scene.ProT_ImageTexturePath
         try:
             img = bpy.data.images.load(realpath)
         except:
@@ -89,20 +121,22 @@ class ProT_PaintVertices(bpy.types.Operator):
         #http://blender.stackexchange.com/questions/3673/why-is-accessing-image-data-so-slow
         pix = img.pixels[:]
         
-        realpath2 = os.path.expanduser('~/Documents/Blender/ProjectionTools/noise.png')
-        try:
-            img2 = bpy.data.images.load(realpath2)
-        except:
-            raise NameError("Cannot load image %s" % realpath2)
+        if context.scene.ProT_use_noise_tex:
+            realpath2 = context.scene.ProT_NoiseTexturePath
+            try:
+                img2 = bpy.data.images.load(realpath2)
+            except:
+                raise NameError("Cannot load image %s" % realpath2)
             
-        #store image data in a list since texture lookup is slowwwwww
-        #http://blender.stackexchange.com/questions/3673/why-is-accessing-image-data-so-slow
-        noisepix = img2.pixels[:]
+            #store image data in a list since texture lookup is slowwwwww
+            #http://blender.stackexchange.com/questions/3673/why-is-accessing-image-data-so-slow
+            noisepix = img2.pixels[:]
         
         if context.scene.ProT_rotate_texture_90 == True:
             print("TODO!!!!, reorder values in pix list so that input image is effectively rotated")
         
-
+        end = time.time()
+        print('Image Storage Time : ' + str(end - start))
         ##############################################################################################
         #http://blender.stackexchange.com/questions/32584/set-vertex-normals-to-vertex-color-in-python
         ##############################################################################################
@@ -129,16 +163,12 @@ class ProT_PaintVertices(bpy.types.Operator):
         YNoiseStrength = context.scene.ProT_Ynoise_scale
         
         start = time.time()
+        
         for poly in polygons:
                 for loop_index in poly.loop_indices:
                     loop_vert_index = mesh.loops[loop_index].vertex_index
 
                     color = [1 , 1, 0]
-
-                    #vcol_layer.data[loop_index].color = color
-                    
-                    #vcol_layer.data[loop_index].color = mesh.vertices[loop_vert_index].normal
-                    #color = mesh.vertices[loop_vert_index].normal
                     
                     #get vertex global/local positions
                     #http://blender.stackexchange.com/questions/1311/how-can-i-get-vertex-positions-from-a-mesh
@@ -161,7 +191,12 @@ class ProT_PaintVertices(bpy.types.Operator):
                     workingPosY = vPos.y
                     
                     channelOffset = 0       #since we're working with black and white
-                    noise = 1#noisepix[ 4 * ((int(workingPosX) + imageWidth * int(workingPosY))) + channelOffset]
+                    
+                    if context.scene.ProT_use_noise_tex:
+                        noise = 1#noisepix[ 4 * ((int(workingPosX) + imageWidth * int(workingPosY))) + channelOffset]
+                    
+                    else:
+                        noise = 1
                     
                     workingPosX = math.pow(abs(workingPosX), (XNoiseStrength * noise))
                     workingPosY = math.pow(abs(workingPosY), (YNoiseStrength * noise))
@@ -193,16 +228,15 @@ class ProT_PaintVertices(bpy.types.Operator):
 
                     #print("painting vert",loop_index, "to color ", color[0], color[1], color[2])
 
-
+        mesh.update()
         end = time.time()
         print('Vertex Painting Time : ' + str(end - start))
-        mesh.update()
-        
+                
         bpy.ops.object.mode_set(mode='VERTEX_PAINT')
         return{"FINISHED"}
     
     
-    
+#create a copy of the mesh and displace the vertices    
 
 class ProT_DisplaceVertices(bpy.types.Operator):
     bl_idname = "mesh.prot_displace_vertices"
@@ -211,7 +245,7 @@ class ProT_DisplaceVertices(bpy.types.Operator):
     
     def invoke(self, context, event):
         
-        
+        start = time.time()
 
         offset = context.scene.ProT_disp_scale
 
@@ -258,8 +292,6 @@ class ProT_DisplaceVertices(bpy.types.Operator):
 
         vcol_averages = {k: avg_col(v) for k, v in tk.items()}
         
-
-        print('Displacement Time!! - switch to edit mode')
         bpy.ops.object.mode_set(mode='EDIT')
         
         #modify the mesh
@@ -274,15 +306,121 @@ class ProT_DisplaceVertices(bpy.types.Operator):
         # Finish up, write the bmesh back to the mesh
         bm.to_mesh(mesh)
         
-        return{"FINISHED"}
+        end = time.time()
+        print('Dispalcement Time : ' + str(end - start))
         
+        return{"FINISHED"}
+    
+#Displace the mesh by using bmesh to move the verts in the orignal mesh without making a copy
+#will cause a crash if the bmesh is tesselated or the number of verts is changed
+#http://blender.stackexchange.com/questions/35278/cant-write-bmesh-object-back-to-mesh-datablock
+
+class ProT_DisplaceVertices_method2(bpy.types.Operator):
+    bl_idname = "mesh.prot_displace_vertices_method2"
+    bl_label = "Paint Vertices"
+    bl_options = {"UNDO"}
+    
+    def invoke(self, context, event):
+        
+        start = time.time()
+
+        offset = context.scene.ProT_disp_scale
+
+        #Get a reference to our working mesh
+        current_obj = bpy.context.active_object 
+        mesh = current_obj.data
+                 
+        if mesh.vertex_colors:
+            vcol_layer = mesh.vertex_colors.active
+        else:
+            vcol_layer = mesh.vertex_colors.new()
+
+        mesh.vertex_colors.active = vcol_layer
+        
+        ##############################################################################################
+        #Store our average vertex colors in a dictionary
+        #Get all relevant colors for each vertex and average them
+        #http://blender.stackexchange.com/questions/2162/select-vertex-if-vertex-color-is-dark
+        ##############################################################################################
+
+        tk = defaultdict(list)
+
+        color_layer = mesh.vertex_colors['Col']
+        
+        i = 0
+        for poly in mesh.polygons:
+            for idx in poly.loop_indices:
+                loop = mesh.loops[idx]
+                color = color_layer.data[i].color
+                tk[loop.vertex_index].append(color)
+                i += 1
+        
+        #function to average a list of colors for a single vertex
+        def avg_col(cols):
+            avg_col = Color((0.0, 0.0, 0.0))
+            #avg_col = [0.0, 0.0, 0.0]
+            for col in cols:
+                avg_col += col/len(cols)
+            return avg_col
+
+        vcol_averages = {k: avg_col(v) for k, v in tk.items()}
+        
+        bpy.ops.object.mode_set(mode='EDIT')
+        # Get a BMesh representation of the active mesh
+        bm = bmesh.from_edit_mesh(mesh)   # create a bmesh copy
+        
+        #modify the mesh
+        for i, vert in enumerate (bm.verts):
+            vertOffset = vert.normal * offset * vcol_averages[i].r
+
+            #vert.co = vert.co + vertOffset
+            vert.co += vertOffset 
+                    
+        # Update mesh, so we see the changes.
+        bmesh.update_edit_mesh(mesh, False, False)
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+        # Because we did not add or remove geometry,
+        # tessface=False and destructive=False can be used.
+        # DO NOT DO THIS IF YOU CHANGED GEOMETRY!
+        # Blender will crash otherwise.
+        
+        end = time.time()
+        print('Dispalcement Time : ' + str(end - start))
+        
+        return{"FINISHED"}
+    
+    
 #call the register class upon initialization to simplify installation as an addon
 #and to create scene variable which classes can reference
 def register():
-    bpy.utils.register_class(ProjectionToolsPanel)
+    bpy.utils.register_class(ProToolsProjectionPanel)
+    bpy.utils.register_class(ProToolsDisplacementPanel)
     bpy.utils.register_class(ProT_PaintVertices)
     bpy.utils.register_class(ProT_DisplaceVertices)
+    bpy.utils.register_class(ProT_DisplaceVertices_method2)
     
+    bpy.types.Scene.ProT_ImageTexturePath = bpy.props.StringProperty(name="Browse Image:",
+        #attr="main_image_path",# this a variable that will set or get from the scene
+        description="Main image texture file path",
+        maxlen= 1024,
+        subtype='FILE_PATH',
+        default= "")#this set the text
+        
+    bpy.types.Scene.ProT_NoiseTexturePath = bpy.props.StringProperty(name="Browse Image:",
+        #attr="noise_image_path",# this a variable that will set or get from the scene
+        description="Noise texture file path",
+        maxlen= 1024,
+        subtype='FILE_PATH',
+        default= "")#this set the text
+    
+    bpy.types.Scene.ProT_use_noise_tex = bpy.props.BoolProperty \
+        (
+        name = "Use noise texture",
+        description = "Multiplies the noise strength by a noise texture. 1 otherwise.",
+        default = False
+        )
+        
     bpy.types.Scene.ProT_rotate_texture_90 = bpy.props.BoolProperty \
         (
         name = "Rotate Texture 90 Degrees",
@@ -321,10 +459,14 @@ def register():
     
     
 def unregister():
-    bpy.utils.unregister_class(ProjectionToolsPanel)
+    bpy.utils.unregister_class(ProToolsProjectionPanel)
+    bpy.utils.unregister_class(ProToolsDisplacementPanel)
     bpy.utils.unregister_class(ProT_PaintVertices)
     bpy.utils.unregister_class(ProT_DisplaceVertices)
+    bpy.utils.unregister_class(ProT_DisplaceVertices_method2)
     
+    del bpy.types.Scene.ProT_ImageTexturePath
+    del bpy.Types.Scene.ProT_NoiseTexturePath
     del bpy.types.Scene.ProT_rotate_texture_90
     del bpy.types.ProT_texture_scale
     del bpy.types.ProT_disp_scale
@@ -333,3 +475,14 @@ def unregister():
 #boilerplate which will invoke our registration routine in the situations (like the Text Editor) where Blender doesn't do it for us:
 if __name__ == "__main__":
     register()
+    
+    
+    
+##############################################################################################
+#Notes
+##############################################################################################
+'''
+Image preview icons 
+http://blender.stackexchange.com/questions/32335/how-to-implement-custom-icons-for-my-script-addon
+
+'''
